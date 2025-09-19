62d2d78f3b5c77ddb9f779a62e85c1d922608728
Slobosha <lehaleh69@gmail.com>
2025-09-19 19:47:19 +0300

    Code.py

--- code.py
+++ code.py
@@ -1,4 +1,3 @@
-from itertools import *
 import unittest
 
 def twosum(nums, target):
@@ -11,11 +10,11 @@ def twosum(nums, target):
     return None
 
 class TestMath(unittest.TestCase):
-    def example1(self):
+    def test_example1(self):
         self.assertEqual(twosum([2,7,11,15], 9), [0, 1])
-    def example2(self):
+    def test_example2(self):
         self.assertEqual(twosum([3,2,4], 6), [1, 2])
-    def example3(self):
+    def test_example3(self):
         self.assertEqual(twosum([3, 3], 6), [0, 1])
 
 unittest.main(argv=[''], verbosity=2, exit=False)
